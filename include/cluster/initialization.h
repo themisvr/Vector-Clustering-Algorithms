282a94231783d98212bcb8c63d03ca00d8080e13
Lazaros Avgeridis <lazarosavgeridis@gmail.com>
2020-10-20 02:52:38 +0300

    some changes on reverse assignment (hypercube)

--- include/cluster/initialization.h
+++ include/cluster/initialization.h
@@ -7,64 +7,7 @@
 #include <random>
 
 #include "../modules/exact_nn/exact_nn.h"
-
-
-size_t binary_search(const std::vector<std::pair<float, size_t>> &partial_sums, size_t sz, float val)
-{
-
-    size_t middle = 0, begin = 0, end = sz;
-    const std::pair<float, size_t> *less_than = &partial_sums[0];
-    const std::pair<float, size_t> *greater_than = &partial_sums[0];
-
-    while (begin <= end) {
-
-        middle = begin + (end - begin) / 2;
-        if (val == partial_sums[middle].first) {
-            return partial_sums[middle].second;
-        }
-        if(val < partial_sums[middle].first) {
-            less_than = &partial_sums[middle];
-            end = middle - 1;
-
-        }
-        else {
-            greater_than = &partial_sums[middle];
-            begin = middle + 1;
-
-        }
-    }
-
-    std::cout << "P(r-1) = " << greater_than->first << " < " << val << " <= P(r) = " << less_than->first << std::endl;
-
-    return less_than->second;
-}
-
-
-float find_max(const std::vector<float> &min_distances)
-{
-    float max_dist = std::numeric_limits<float>::min();
-
-    for (float dist : min_distances) {
-        if (dist > max_dist) max_dist = dist;
-    }
-
-    return max_dist;
-}
-
-
-void normalize_distances(std::vector<float> &min_distances)
-{
-    float dmax = find_max(min_distances);
-
-    for (float &d : min_distances)
-        d /= dmax;
-}
-
-
-static bool compare(const std::pair<float, size_t> &p1, const std::pair<float, size_t> &p2) 
-{
-    return p1.first < p2.first;
-}
+#include "../cluster/cluster_utils.h"
 
 
 template<typename T>
@@ -78,6 +21,12 @@ bool in(const std::vector<std::pair<std::vector<T>, size_t>> &centroids, size_t 
     }
     
     return false;
+}
+
+
+bool compare(const std::pair<float, size_t> &p1, const std::pair<float, size_t> &p2) 
+{
+    return p1.first < p2.first;
 }
 
 
@@ -131,7 +80,7 @@ std::vector<std::pair<std::vector<T>, size_t>> init_plus_plus(const std::vector<
         std::uniform_real_distribution<float> distribution(0.0, new_partial_sum);
         float x = distribution(generator);
         std::sort(partial_sums.begin(), partial_sums.end(), compare);
-        size_t r = binary_search(partial_sums, partial_sums.size(), x);
+        size_t r = binary_search(partial_sums, x);
         std::cout << r << std::endl;
         centroids.emplace_back(train_samples[r], r);
         partial_sums.clear();   // next iteration: partial_sum's size will be decreased by one
--- src/cluster/cluster_utils.cpp
+++ src/cluster/cluster_utils.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <utility>
 #include <unistd.h>
 
 #include "../../include/io_utils/io_utils.h"
@@ -62,4 +63,56 @@ void parse_cluster_args(int argc, char * const argv[], cluster_args *args) {
                 break;
         }
     }
+}
+
+
+size_t binary_search(const std::vector<std::pair<float, size_t>> &partial_sums, float val)
+{
+
+    size_t middle = 0, begin = 0, end = partial_sums.size();
+    const std::pair<float, size_t> *less_than = &partial_sums[0];
+    const std::pair<float, size_t> *greater_than = &partial_sums[0];
+
+    while (begin <= end) {
+
+        middle = begin + (end - begin) / 2;
+        if (val == partial_sums[middle].first) {
+            return partial_sums[middle].second;
+        }
+        if(val < partial_sums[middle].first) {
+            less_than = &partial_sums[middle];
+            end = middle - 1;
+
+        }
+        else {
+            greater_than = &partial_sums[middle];
+            begin = middle + 1;
+
+        }
+    }
+
+    std::cout << "P(r-1) = " << greater_than->first << " < " << val << " <= P(r) = " << less_than->first << std::endl;
+
+    return less_than->second;
+}
+
+
+float find_max(const std::vector<float> &min_distances)
+{
+    float max_dist = std::numeric_limits<float>::min();
+
+    for (float dist : min_distances) {
+        if (dist > max_dist) max_dist = dist;
+    }
+
+    return max_dist;
+}
+
+
+void normalize_distances(std::vector<float> &min_distances)
+{
+    float dmax = find_max(min_distances);
+
+    for (float &d : min_distances)
+        d /= dmax;
 }
